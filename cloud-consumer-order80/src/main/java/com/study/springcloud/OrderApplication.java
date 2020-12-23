486af334b4b9a1970461a2a92bb75fc85020bc70
lijingyu <longxinshenmde@163.com>
2020-12-23 14:22:22 +0800

    提交消费者，注册中心

--- cloud-consumer-order80/src/main/java/com/study/springcloud/OrderApplication.java
+++ cloud-consumer-order80/src/main/java/com/study/springcloud/OrderApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @Author lijingyu
+ * @Date 2020/12/23 11:29
+ * @Description
+ * @Version 1.0
+ */
 @SpringBootApplication
 @EnableEurekaClient
-public class PaymentMain8001 {
+public class OrderApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(OrderApplication.class,args);
     }
 }
---
+++ cloud-consumer-order80/src/main/java/com/study/springcloud/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.study.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author lijingyu
+ * @Date 2020/12/23 11:30
+ * @Description
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class OrderController {
+
+
+}
